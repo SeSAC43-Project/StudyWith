8a217160b620536398bdad613cba3730c342ccdb
이주옥 <juok9802@gmail.com>
2022-08-16 22:05:26 +0900

    feat: update routes/index.js, signUpController.js file

--- Swith/routes/index.js
+++ Swith/routes/index.js
@@ -2,16 +2,17 @@ const express = require('express');
 const UserRouter = express.Router();
 const login = require('../controller/LoginController');
 const signUp = require('../controller/SignUpController');
-const multer = require('multer'); 
+const multer = require('multer');
 
+/* 프로필 이미지 업로드 */
 const profileUpload = multer({
     storage: multer.diskStorage({
         destination(req, file, cb) {
-            cb(null, 'public/'); // 파일 저장위치: public/
+            cb(null, 'public/user'); // 파일 저장위치: public/user
         },
         filename(req, file, cb) {
             const ext = path.extname(file.originalname);
-            cb(null, req.body.id + Date.now() + ext ); // 파일이름: id+현재날짜.확장자
+            cb(null, req.body.user_id + Date.now() + ext ); // 파일이름: id+현재날짜.확장자
         },
     }),
     limits: { fileSize: 5*1024*1024 },
@@ -19,10 +20,14 @@ const profileUpload = multer({
 
 /* 로그인 관련 */
 UserRouter.get('/login', login.login_index); 
+// UserRouter.post('/login',login.post_login);
 
 /* 회원가입 관련 */
-UserRouter.post('/signup', signUp.post_user);
-UserRouter.post('/signup/upload', profileUpload.single('view input value name'), signUp.uploadProfile);
+UserRouter.get('/signup', signUp.signUp_index); //회원가입 화면
+UserRouter.post('/signup', signUp.post_user); // 회원가입
+UserRouter.post('/signup/upload', profileUpload.single('view input value name'), signUp.uploadProfile); // 프로필 이미지 업로드
+UserRouter.post('/signup/isId', signUp.isId); // 아이디 중복검사
+UserRouter.post('/signup/isName', signUp.isName); // 닉네임 중복검사
 
 
 module.exports = UserRouter;
