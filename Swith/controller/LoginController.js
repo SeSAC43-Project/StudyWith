b40ebecbf5ee43a5dc422657d7c6b7730cfb5e1b
SihaGwak <sasha.gwak@gmail.com>
2022-08-17 18:35:03 +0900

    feat: password route create

--- Swith/controller/LoginController.js
+++ Swith/controller/LoginController.js
@@ -1,8 +1,10 @@
 const Models  = require('../model');
 
+// 로그인 화면 렌더링
 exports.login_index = (req, res) => {
     res.render('login'); 
 }
+
 
 // 로그인 시스템 구현 
 exports.post_login = (req, res) => {
@@ -17,4 +19,24 @@ exports.post_login = (req, res) => {
         }
         res.send('로그인 실패'); 
     });
+}
+
+// 비밀번호 찾기 화면 렌더링
+exports.find_index = (req, res) => {
+    res.render('find'); 
+}
+
+// 비밀번호 찾기 구현 
+exports.post_find = (req, res) => {
+    console.log('비번찾기');
+}
+
+// 비밀번호 변경 화면 렌더링
+exports.modify_index = (req, res) => {
+    res.render('modify'); 
+}
+
+// 비밀번호 변경 구현 
+exports.post_modify = (req, res) => {
+    console.log('비번변경');
 }
