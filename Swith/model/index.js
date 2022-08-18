f0f8db813db4e605edeb5b0e8c0196326ffd5608
juoklee <juok9802@gmail.com>
2022-08-18 17:38:48 +0900

    feat: study  model 생성 및 관련 파일 수정

--- Swith/model/index.js
+++ Swith/model/index.js
@@ -10,7 +10,27 @@ const sequelize = new Sequelize(
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
-//User.js 모델의 (Sequelize, DataTypes)
+
 db.User = require("./User")(sequelize, Sequelize);
+db.Studygroup = require("./Studygroup")(sequelize, Sequelize);
+
+/* foreign key설정 */
+db.Studygroup.hasMany(db.User, {
+    foreignKey: 'user_id',
+    allowNull: false,
+    constraints: true,
+    onDelete: 'cascade'
+})
+db.User.belongsTo(db.Studygroup, {
+    foreignKey: 'user_id'
+});
+// User.sync({
+//     force: process.env.TABLE_CREATE_ALWAYS === 'true', // true : (drop) table 데이터 없어질 수 있음
+//     alter: process.env.TABLE_ALTER_SYNC === 'true'     // 개발 끝나면 false로 하기
+// })
+// Studygroup.sync({
+//     force: process.env.TABLE_CREATE_ALWAYS === 'true', // true : (drop) table 데이터 없어질 수 있음
+//     alter: process.env.TABLE_ALTER_SYNC === 'true'     // 개발 끝나면 false로 하기
+// })
 
 module.exports = db;
