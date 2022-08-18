745a764686be36a43cc1d40c19db843d52dd14ee
sihagwak <sasha.gwak@gmail.com>
2022-08-19 00:13:34 +0900

    feat: create session

--- Swith/index.js
+++ Swith/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+
 const bodyParser = require("body-parser");
 const multer = require('multer');
 const port = 8000;
@@ -8,6 +9,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
 app.use( "/public", express.static('public')); 
 app.use( bodyParser.json() );
+
 
 /* 회원가입&로그인 경로 */
 const { UserRouter } = require('./routes');
