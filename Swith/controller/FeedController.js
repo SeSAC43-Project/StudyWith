bf95218203f3ae47c0db821ae039a025371965b1
SihaGwak <sasha.gwak@gmail.com>
2022-08-19 17:09:10 +0900

    feat: feed write controller

--- Swith/controller/FeedController.js
+++ Swith/controller/FeedController.js
@@ -4,3 +4,27 @@ const Models  = require('../model');
 exports.write_index = (req, res) => {
     res.render('feed'); 
 }
+
+// 게시물 등록  
+exports.post_write = (req, res) => {
+    let Obj = {
+        head_id : req.body.head_id, 
+        study_category : req.body.study_category, 
+        study_name : req.body.study_name,
+        study_form : req.body.study_form, 
+        study_recruit : req.body.study_recuit, 
+        study_address : req.body.study_address, 
+        study_image : req.body.study_image, 
+        study_content : req.body.study_content, 
+        start_period : req.body.start_period, 
+        end_period : req.body.end_period, 
+        study_regdate : Date.now(), 
+        hashtag : req.body.hashtag, 
+        study_views : 0, 
+    }; 
+    Models.Studygroup.create( Obj )
+    .then((result) => {
+        console.log(result);
+        res.send({id: result.id});
+    })
+}
