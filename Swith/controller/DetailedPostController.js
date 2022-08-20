607b33ccc037e6097321352c19510a63bf0e3b55
이주옥 <juok9802@gmail.com>
2022-08-21 02:17:05 +0900

    feat: detailedPost.ejs, Controller 수정

--- Swith/controller/DetailedPostController.js
+++ Swith/controller/DetailedPostController.js
@@ -4,39 +4,54 @@ const { sequelize } = require("../model/index");
 /* 게시물 상세 조회 화면 렌더링*/
 exports.detailedPost_index = async (req, res) => {
 
-    const headSql = `SELECT M.user_id, U.user_name, U.user_image, G.study_id, G.head_id, G.study_name, G.study_content, G.study_form, G.study_address, G.study_recruit, G.study_category, G.study_image, G.study_content, G.start_period, G.end_period, G.hashtag, G.study_views from user U 
-        inner join studygroup G on G.head_id = U.user_id 
-        inner join studymember M on M.study_id = G.study_id 
-        where U.user_id='${req.query.user_id}' AND G.study_id=${req.query.study_id};`;
+    // 일반가입멤버인지 아닌지 check
+    const [isMember_results, metadata1] = await sequelize.query(`SELECT * from studymember where studymember.user_id='${req.query.user_id}' AND studymember.study_id=${req.query.study_id};`);
 
-    // 그룹장인지 아닌지 검사
-    const [isHead_results, metadata] = await sequelize.query(headSql);
+    if ( isMember_results.length === 0 ) { // 일반가입멤버가 아니면
 
-    if ( isHead_results.length === 0 ) { // 그룹장이 아니면 멤버인지 아닌지 검사
+        // 조장인지 아닌지 check
+        const [isHead_results, metadata3] = await sequelize.query(`SELECT * from studygroup  where studygroup.head_id='${req.query.user_id}' AND studygroup.study_id=${req.query.study_id};`);
 
-        const memberSql = `SELECT M.user_id, U.user_name, U.user_image, G.study_id, G.head_id, G.study_name, G.study_content, G.study_form, G.study_address, G.study_recruit, G.study_category, G.study_image, G.study_content, G.start_period, G.end_period, G.hashtag, G.study_views from user U 
-            inner join studymember M on M.user_id = U.user_id
-            inner join studygroup G on G.study_id = M.study_id 
-            where U.user_id='${req.query.user_id}' AND M.study_id=${req.query.study_id};`;
+        if ( isHead_results.length === 0 ) { // 가입X 이면
 
-        const [isMember_results, metadata1] = await sequelize.query(memberSql); //멤버인지 가입안한 사람인지 검사
-        console.log("isMember_results 값:", isMember_results)
-        const DataSql = `SELECT M.user_id, U.user_name, U.user_image, G.study_id, G.head_id, G.study_name, G.study_content, G.study_form, G.study_address, G.study_recruit, G.study_category, G.study_image, G.study_content, G.start_period, G.end_period, G.hashtag, G.study_views from user U 
-            inner join studymember M on M.user_id = U.user_id
-            inner join studygroup G on G.study_id = M.study_id 
-            where M.study_id=${req.query.study_id};`
+            const [isnotgroup_results, metadata6] = await sequelize.query(
+                `SELECT U.user_id, U.user_name, U.user_image, G.study_id, G.head_id, G.study_name, G.study_content, G.study_form, G.study_address, G.study_recruit, G.study_category, G.study_image, G.study_content, G.start_period, G.end_period, G.hashtag, G.study_views
+                FROM studygroup G
+                LEFT JOIN studymember AS M
+                ON G.study_id = M.study_id
+                LEFT JOIN user AS U
+                    ON U.user_id = M.user_id
+                WHERE G.study_id = ${req.query.study_id}; `);
 
-        const [results, metadata2] = await sequelize.query(DataSql); //보내줄 데이터
+            console.log("가입X result:", isnotgroup_results);
 
-        if (isMember_results.length === 0 ) { // 가입 안한 사람이면
-            return res.render('detailedPost', {result: results, isMember: false})
-        } else { // 멤버이면
-            return res.render('detailedPost', {result: results, isMember: true})
-        }
+            return res.render('detailedPost3', {result: isnotgroup_results});
 
-    } else { // 그룹장이면
-        console.log("isHead_results:", isHead_results);
-        return res.render('detailedPost', {result: isHead_results})
+        } else { // 조장이면
+           
+            const [isHead_results, metadata6] = await sequelize.query(
+                `SELECT U.user_id, U.user_name, U.user_image, G.study_id, G.head_id, G.study_name, G.study_content, G.study_form, G.study_address, G.study_recruit, G.study_category, G.study_image, G.study_content, G.start_period, G.end_period, G.hashtag, G.study_views
+                FROM studygroup G
+                LEFT JOIN studymember AS M
+                ON G.study_id = M.study_id
+                LEFT JOIN user AS U
+                    ON U.user_id = M.user_id
+                WHERE G.study_id = ${req.query.study_id}; `);
+
+            console.log("조장O result:", isHead_results);
+
+            return res.render('detailedPost', {result: isHead_results});
+        }  
+
+    } else { // 일반가입멤버이면
+
+        const [member_data, metadata2] = await sequelize.query(`SELECT M.user_id, U.user_name, U.user_image, G.study_id, G.head_id, G.study_name, G.study_content, G.study_form, G.study_address, G.study_recruit, G.study_category, G.study_image, G.study_content, G.start_period, G.end_period, G.hashtag, G.study_views from user U 
+            inner join studymember M on M.user_id = U.user_id inner join studygroup G on G.study_id = M.study_id where M.study_id=${req.query.study_id};`);
+
+        console.log("일반멤버o result:", member_data);
+
+        return res.render('detailedPost2', {result: member_data});
+        
     }
 }
 
