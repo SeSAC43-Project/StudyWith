2b667009cb8f3db7085c4aa9e56caa16a99e588d
sihagwak <sasha.gwak@gmail.com>
2022-08-21 01:09:09 +0900

    feat: main search

--- Swith/controller/MainController.js
+++ Swith/controller/MainController.js
@@ -4,3 +4,34 @@ const Models  = require('../model');
 exports.main_index = (req, res) => {
     res.render('main'); 
 }
+
+exports.main_search = (req, res) => {
+    if (req.query.search == "") { // 검색어가 없는 경우
+        res.send(''); 
+        return 
+    }
+
+    // 검색어가 있으면 검색 
+    console.log('요긴 컨트롤러',req.query.search);
+
+    Models.Studygroup.findAll({
+        where: {
+            [Models.Op.or] : [
+                {
+                    hashtag : {
+                        [Models.Op.like]: '%'+ req.query.search + '%'
+                    }
+                },
+                {
+                    study_name : {
+                        [Models.Op.like]: '%'+ req.query.search + '%'
+                    }
+                }
+            ]
+        }
+    })
+    .then((result) => {
+        console.log(result);
+        res.send({data: result});
+    })
+}
