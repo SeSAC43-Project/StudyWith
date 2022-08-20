4f39d84240076e202033325720b567e7a2646016
이주옥 <juok9802@gmail.com>
2022-08-20 18:18:03 +0900

    feat: update routes/index.js, detailedPostController file

--- Swith/controller/DetailedPostController.js
+++ Swith/controller/DetailedPostController.js
@@ -1,7 +1,7 @@
 const models = require("../model");
 const { sequelize } = require("../model/index"); 
 
-/* 게시물 상세 조회 화면 */
+/* 게시물 상세 조회 화면 렌더링*/
 exports.detailedPost_index = async (req, res) => {
 
     // 그룹장인지 아닌지 검사
@@ -11,8 +11,8 @@ exports.detailedPost_index = async (req, res) => {
 
     if (isHead_results == null ) { // 그룹장이 아니면 멤버인지 아닌지 검사
         const isMember_results = await sequelize.query(`SELECT * from user 
-        inner join studymember on studymember.user_id=user.user_id 
-        where user.user_id='${req.query.user_id}' AND studymember.study_id=${req.query.study_id};`);
+            inner join studymember on studymember.user_id=user.user_id 
+            where user.user_id='${req.query.user_id}' AND studymember.study_id=${req.query.study_id};`);
         
         await models.Studygroup.findOne({
             where: {study_id: req.query.study_id}
@@ -27,29 +27,45 @@ exports.detailedPost_index = async (req, res) => {
         await models.Studygroup.findOne({
             where: {study_id: req.query.study_id}
         }).then((result) => {
-            console.log(result);
+            console.log("result:", result);
             return res.render('detailedPost', {result: result, isMember: true, isHead: true})
         });
     }
 }
 
 
-/* 가입하기 버튼 클릭 시 정보 저장*/
-exports.post_reggroup =  (req, res) => {
-    console.log("post_reggroup: ", req.body);
+/* 조장: 게시물 수정하러가기 클릭 시 정보 저장*/
+exports.get_editgroup =  (req, res) => {
+
+    models.Studygroup.findOne({
+        where: {study_id: req.query.study_id}
+    }).then((result) => {
+        console.log('Studygroup 정보:', result); 
+        res.render(); // 게시물 수정화면으로 이동(아직 미정)
+    });
+}
+
+/* 일반멤버: 탈퇴하기 버튼 클릭시 정보 삭제*/
+exports.delete_leavegroup =  (req, res) => {
+
+    models.Studymember.destroy({ where: { 
+        user_id: req.body.user_id,
+        study_id: req.body.study_id } })
+    .then((result) => {
+        res.send("탈퇴완료");
+    })
+}
+
+/* 가입안한 사람: 가입하기 버튼 클릭 시 정보 저장*/
+exports.post_joingroup =  (req, res) => {
 
     let object = {
         user_id: req.body.user_id,
         study_id: req.body.study_id
     };
 
-    models.Studygroup.create( object )
+    models.Studymember.create( object )
     .then((result) => {
-        res.send(result);
+        res.send("가입완료");
     })
-}
-
-/* 조회수 정보 저장 */
-exports.post_views = (req, res) => {
-
 }
