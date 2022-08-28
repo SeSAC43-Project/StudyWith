68b603ce9018bbc8357d6e6da65ddae39f8fdfb8
sihagwak <sasha.gwak@gmail.com>
2022-08-28 16:11:43 +0900

    mypage current member append

--- Swith/controller/MypageController.js
+++ Swith/controller/MypageController.js
@@ -64,6 +64,26 @@ exports.mypage_studys = async (req,res) => {
     const memberStudy = await Models.sequelize.query(sqlMember);
     console.log('내가 가입한 스터디 :', memberStudy);
 
+    // 내가 가입한 스터디 현재인원 넣을 리스트 생성
+    var memberCount = []
+    const member_data = memberStudy[0];
+
+    // 내가 가입한 스터디 현재 가입인원 확인
+    for (let i=0; i < member_data.length; i++) {
+        let countMember = `
+        SELECT COUNT(M.user_id) + 1 AS member
+        FROM studymember AS M
+        LEFT OUTER JOIN studygroup AS G ON M.study_id = G.study_id
+        WHERE M.study_id = ${member_data[i].study_id};`
+        
+        const result2 = await Models.sequelize.query(countMember);
+        const currentCount = result2[0];
+        memberCount.push(currentCount[0].member);
+    }
+
+    console.log(memberCount);
+
+
     // 보내줄 내가 조장인 스터디 정보
     let sqlHead = `
     SELECT G.*
@@ -74,12 +94,33 @@ exports.mypage_studys = async (req,res) => {
     const HeadStudy = await Models.sequelize.query(sqlHead);
     console.log('내가 조장인 스터디 : ', HeadStudy);
 
+
+    // 내가 조장인 스터디 현재인원 넣을 리스트 생성
+    var HeadCount = []
+    const Head_data = HeadStudy[0];
+
+    // 내가 가입한 스터디 현재 가입인원 확인
+    for (let i=0; i < Head_data.length; i++) {
+        let countHeadMember = `
+        SELECT COUNT(M.user_id) + 1 AS member
+        FROM studymember AS M
+        LEFT OUTER JOIN studygroup AS G ON M.study_id = G.study_id
+        WHERE M.study_id = ${Head_data[i].study_id};`
+        
+        const result3 = await Models.sequelize.query(countHeadMember);
+        const currentCount = result3[0];
+        HeadCount.push(currentCount[0].member);
+    }
+    console.log(HeadCount);
+
     // 만약 가입한 스터디가 하나도 없다면? 
     var signStudy = true // 가입된 스터디가 있음
     if ( HeadStudy[0].length == 0 && memberStudy[0].length == 0) {
         signStudy = false // 가입된 스터디가 없음
     }
     console.log(signStudy);
-    await res.render('mypage3', {HeadStudy: HeadStudy[0], memberStudy: memberStudy[0], signStudy: signStudy}); 
+
+
+    await res.render('mypage3', {HeadStudy: HeadStudy[0], memberStudy: memberStudy[0], signStudy: signStudy, memberCount: memberCount, HeadCount: HeadCount}); 
 }
 
